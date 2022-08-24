4651f44e7c6af382a1fa1f99dd97a9dd15abd482
Anth Lorenzo <anth@Anths-MacBook-Pro.local>
2022-08-24 14:14:32 -0400

    Development mode stripe integration

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,17 +1,26 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiOutlineShopping } from 'react-icons/ai';
+import Cart from './Cart';
+import { useStateContext } from '../context/StateContext';
 
 const NavBar = () => {
+	const { showCart, setShowCart, totalQuantities } = useStateContext();
+
 	return (
 		<div className="navbar-container">
 			<p className="logo">
 				<Link href="/">Stock Source</Link>
 			</p>
-			<button className="cart-icon" type="button">
+			<button
+				className="cart-icon"
+				type="button"
+				onClick={() => setShowCart(!showCart)}
+			>
 				<AiOutlineShopping />
-				<span className="cart-item-qty">1</span>
+				<span className="cart-item-qty">{totalQuantities}</span>
 			</button>
+			{showCart && <Cart />}
 		</div>
 	);
 };
